c7d722f5815515307c4a7e438c89d860773c605a
peter_henderson <peter.henderson@mail.mcgill.ca>
2012-11-17 14:16:41 -0500

    Fix formatting, add removal of delimiter in getting price
    Also change price to float from int

--- src/ActionThread.java
+++ src/ActionThread.java
@@ -14,174 +14,188 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.json.JSONException;
 import org.json.JSONObject;
-/**
-* Thread which takes action methods sends information to the server and creates JSON
-* @author TeamG
-**/
-
-public class ActionThread extends Thread{
-	Queue<ActionObject> q;
-	JSONObject json;
-	Socket skt;
-	DataOutputStream dOut;
-	BufferedReader dIn;
-	boolean loop = true;
 
 /**
-*	Constructor for the thread, sets up socket connection and Data Streams, also begins JSON
-* @author TeamG
-**/
-	public ActionThread(){
-		json = new JSONObject();
-		try{
-                    json.put("team","Team G");
-                    json.put("destination", "mcgillcodejam2012@gmail.com");
-                }catch(JSONException e){
-                    e.printStackTrace();
+ * Thread which takes action methods sends information to the server and creates
+ * JSON
+ *
+ * @author TeamG
+*
+ */
+public class ActionThread extends Thread {
+
+    Queue<ActionObject> q;
+    JSONObject json;
+    Socket skt;
+    DataOutputStream dOut;
+    BufferedReader dIn;
+    boolean loop = true;
+
+    /**
+     * Constructor for the thread, sets up socket connection and Data Streams,
+     * also begins JSON
+     *
+     * @author TeamG
+*
+     */
+    public ActionThread() {
+        json = new JSONObject();
+        try {
+            json.put("team", "Team G");
+            json.put("destination", "mcgillcodejam2012@gmail.com");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        q = new LinkedList<ActionObject>();
+        try {
+            skt = new Socket("localhost", 1234);
+            dOut = new DataOutputStream(skt.getOutputStream());
+            dIn = new BufferedReader(new InputStreamReader(skt.getInputStream()));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Sends a buy order to the server, adds to JSON
+     *
+     * @author TeamG
+*
+     */
+    private ActionObject buy(ActionObject buy) throws IOException, JSONException {
+        float price = 0;
+        dOut.writeChar(66);
+
+        int time = buy.getTime();
+        String strategy = buy.getStrategy();
+
+        String input = dIn.readLine();
+        input = input.replace("|", "");
+        try {
+            price = java.lang.Float.parseFloat(input);
+        } catch (NumberFormatException e) {
+            e.printStackTrace(); //The price sent isn't a price, so an error occurred
+        }
+
+        String manager;
+        manager = managerLookup(); // need to get method
+
+        JSONObject transaction = new JSONObject();
+        transaction.put("time", time);
+        transaction.put("type", "buy");
+        transaction.put("price", price);
+        transaction.put("manager", manager);
+        transaction.put("strategy", strategy);
+        json.accumulate("transactions", transaction);
+        return buy;
+    }
+
+    /**
+     * Sends a sell order to the server, adds to JSON
+     *
+     * @author TeamG
+*
+     */
+    private ActionObject sell(ActionObject sell) throws IOException, JSONException {
+        float price = 0;
+        dOut.writeChar(83);
+
+        int time = sell.getTime();
+        String strategy = sell.getStrategy();
+
+        String input = dIn.readLine();
+        input = input.replace("|", "");
+        try {
+            price = java.lang.Float.parseFloat(input);
+        } catch (NumberFormatException e) {
+        }
+
+        String manager;
+        manager = managerLookup(); // need to get method
+
+        JSONObject transaction = new JSONObject();
+        transaction.put("time", time);
+        transaction.put("type", "sell");
+        transaction.put("price", price);
+        transaction.put("manager", manager);
+        transaction.put("strategy", strategy);
+        json.accumulate("transactions", transaction);
+        return sell;
+    }
+
+    /**
+     * Ends the socket connection and datastreams, saves the json to a .txt file
+     *
+     * @author TeamG
+*
+     */
+    private void end() throws IOException, JSONException {
+        loop = false;
+        dOut.flush();
+        dOut.close();
+        dIn.close();
+        skt.close();
+
+        String jsonString = this.json.toString(2);
+        Writer output = null;
+        File file = new File("json.txt");
+        output = new BufferedWriter(new FileWriter(file));
+        output.write(jsonString);
+        output.close();
+    }
+
+    /**
+     * Adds an action to the running Queue of actions
+     *
+     * @author TeamG
+*
+     */
+    public ActionObject addAction(String action, String strategy, int time) {
+        ActionObject newaction = new ActionObject(action, strategy, time);
+        q.add(newaction);
+        return newaction;
+    }
+
+    /**
+     * Sets up the runnable loop which checks whether the Queue is empty,
+     * dequeues the next action if there is one
+     *
+     * @author TeamG
+*
+     */
+    public void run() {
+        while (loop) {
+            if (q.peek() != null) {
+                ActionObject currentAction = q.remove();
+                if ("BUY".equals(currentAction.getType())) {
+                    try {
+                        this.buy(currentAction);
+                    } catch (IOException ex) {
+                        Logger.getLogger(ActionThread.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (JSONException ex) {
+                        Logger.getLogger(ActionThread.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                } else if ("SELL".equals(currentAction.getType())) {
+                    try {
+                        this.sell(currentAction);
+                    } catch (IOException ex) {
+                        Logger.getLogger(ActionThread.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (JSONException ex) {
+                        Logger.getLogger(ActionThread.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                } else if ("END".equals(currentAction.getType())) {
+                    try {
+                        this.end();
+                    } catch (IOException ex) {
+                        Logger.getLogger(ActionThread.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (JSONException ex) {
+                        Logger.getLogger(ActionThread.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                 }
-                
-		q = new LinkedList<ActionObject>();
-		try{
-			skt = new Socket("localhost", 1234);
-			dOut = new DataOutputStream(skt.getOutputStream());
-			dIn = new BufferedReader(new InputStreamReader(skt.getInputStream()));
-		} catch (UnknownHostException e){
-			e.printStackTrace();
-		} catch (IOException e){
-			e.printStackTrace();
-		}
-	}
-
-/**
-* Sends a buy order to the server, adds to JSON
-* @author TeamG
-**/
-private ActionObject buy(ActionObject buy) throws IOException, JSONException{
-		int price = 0;
-		dOut.writeChar(66);
-
-		int time = buy.getTime();
-		String strategy = buy.getStrategy();
-
-		String input = dIn.readLine();
-		try{
-			price = java.lang.Integer.parseInt(input);
-		}
-		catch (NumberFormatException e){
-			e.printStackTrace(); //The price sent isn't a price, so an error occurred
-		}
-
-		String manager;
-		manager = managerLookup(); // need to get method
-		
-		JSONObject transaction = new JSONObject();
-		transaction.put("time", time);
-		transaction.put("type", "buy");
-		transaction.put("price", price);
-		transaction.put("manager", manager);
-		transaction.put("strategy", strategy);
-		json.accumulate("transactions", transaction);
-                return buy;
-	}
-
-/**
-* Sends a sell order to the server, adds to JSON
-* @author TeamG
-**/
-private ActionObject sell(ActionObject sell) throws IOException, JSONException{
-		int price = 0;
-		dOut.writeChar(83);
-
-		int time = sell.getTime();
-		String strategy = sell.getStrategy();
-
-		String input = dIn.readLine();
-		try{
-			price = java.lang.Integer.parseInt(input);
-		}
-		catch (NumberFormatException e){
-			e.printStackTrace(); //The price sent isn't a price, so an error occurred
-		}
-
-		String manager;
-		manager = managerLookup(); // need to get method
-		
-		JSONObject transaction = new JSONObject();
-		transaction.put("time", time);
-		transaction.put("type", "sell");
-		transaction.put("price", price);
-		transaction.put("manager", manager);
-		transaction.put("strategy", strategy);
-		json.accumulate("transactions", transaction);
-                return sell;
-}
-
-/**
-* Ends the socket connection and datastreams, saves the json to a .txt file
-* @author TeamG
-**/
-private void end() throws IOException, JSONException{
-		loop = false;
-		dOut.flush();
-		dOut.close();
-		dIn.close();
-		skt.close();
-
-		String jsonString = this.json.toString(2);
-		Writer output = null;
-		File file = new File("json.txt");
-		output = new BufferedWriter(new FileWriter(file));
-		output.write(jsonString);
-		output.close();
-	}
-
-/**
-* Adds an action to the running Queue of actions
-* @author TeamG
-**/
-public ActionObject addAction(String action, String strategy, int time){
-		ActionObject newaction = new ActionObject(action, strategy, time);
-		q.add(newaction);
-                return newaction;
-	}
-/**
-* Sets up the runnable loop which checks whether the Queue is empty, dequeues the next action if there is one
-* @author TeamG
-**/
-	public void run(){
-		while(loop){
-			if (q.peek()!=null){
-				ActionObject currentAction = q.remove();
-				if(currentAction.getType() == "BUY"){
-                                try {
-                                    this.buy(currentAction);
-                                } catch (IOException ex) {
-                                    Logger.getLogger(ActionThread.class.getName()).log(Level.SEVERE, null, ex);
-                                } catch (JSONException ex) {
-                                    Logger.getLogger(ActionThread.class.getName()).log(Level.SEVERE, null, ex);
-                                }
-				}
-				else if(currentAction.getType() == "SELL"){
-                                try {
-                                    this.sell(currentAction);
-                                } catch (IOException ex) {
-                                    Logger.getLogger(ActionThread.class.getName()).log(Level.SEVERE, null, ex);
-                                } catch (JSONException ex) {
-                                    Logger.getLogger(ActionThread.class.getName()).log(Level.SEVERE, null, ex);
-                                }
-				}
-				else if(currentAction.getType() == "END"){
-                                try {
-                                    this.end();
-                                } catch (IOException ex) {
-                                    Logger.getLogger(ActionThread.class.getName()).log(Level.SEVERE, null, ex);
-                                } catch (JSONException ex) {
-                                    Logger.getLogger(ActionThread.class.getName()).log(Level.SEVERE, null, ex);
-                                }
-				}
-			}
-		}
-	}
-
-
+            }
+        }
+    }
 }
