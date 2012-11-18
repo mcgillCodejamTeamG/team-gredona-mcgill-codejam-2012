4dfda29d2eedc4420d1ce95b4ea5b73f5711d265
peter_henderson <peter.henderson@mail.mcgill.ca>
2012-11-17 19:53:22 -0500

    filename change for json
    asd

--- src/ActionThread.java
+++ src/ActionThread.java
@@ -116,7 +116,7 @@ public class ActionThread extends Thread {
 
         String jsonString = this.json.toString(2);
         Writer output = null;
-        File file = new File("json"+jsonCounter+".json");
+        File file = new File("codejam.json");
         jsonCounter++;
         output = new BufferedWriter(new FileWriter(file));
         output.write(jsonString);
