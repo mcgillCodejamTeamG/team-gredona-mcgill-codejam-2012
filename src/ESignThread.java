5aeea0c10550839c3fda648215327adcac11111a
peter_henderson <peter.henderson@mail.mcgill.ca>
2012-11-17 19:12:36 -0500

    Fixes of the Esgin
    Esign

--- src/ESignThread.java
+++ src/ESignThread.java
@@ -1,12 +1,13 @@
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.json.JSONObject;
 
 
 /**
@@ -14,9 +15,18 @@ import java.util.logging.Logger;
  * @author TeamG
  */
 public class ESignThread extends Thread {
+    private String JSONString;
+    private String responseMessage;
     
     public ESignThread(){
         
+    }
+    public String getResponseMessage(){
+        return this.responseMessage;
+    }
+    public String setJSONString(JSONObject object){
+        JSONString = object.toString();
+        return JSONString;
     }
     public void run(){
         String type = "application/json";
@@ -30,8 +40,16 @@ public class ESignThread extends Thread {
             conn.setRequestMethod( "POST" );
             conn.setRequestProperty("Authorization", "Basic Y29kZWphbTpBRkxpdGw0TEEyQWQx"); 
             conn.setRequestProperty( "Content-Type", type );
-            OutputStream os = conn.getOutputStream();
-            InputStream is = conn.getInputStream();
+            OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+            writer.write(JSONString);
+            writer.close();
+            if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                String returnString = conn.getResponseMessage();
+                this.responseMessage = returnString;
+            } else {
+            // Server returned HTTP error code.
+                System.out.println("There's a problem with the response");
+            }
              } catch (IOException ex) {
                 Logger.getLogger(ESignThread.class.getName()).log(Level.SEVERE, null, ex);
             } } catch (MalformedURLException ex) {
