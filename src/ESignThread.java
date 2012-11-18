62cf5648a53c86f6a6d319581ad078ec818e1165
peter_henderson <peter.henderson@mail.mcgill.ca>
2012-11-17 21:16:25 -0500

    ESigning DONE
    Also added setter for JSON in Esign, and a Commented out test file for
    the esign

--- src/ESignThread.java
+++ src/ESignThread.java
@@ -1,5 +1,8 @@
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
@@ -7,52 +10,68 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.json.JSONException;
 import org.json.JSONObject;
-
 
 /**
  * Creates new thread for esigning the data
+ *
  * @author TeamG
  */
 public class ESignThread extends Thread {
+
     private String JSONString;
-    private String responseMessage;
-    
-    public ESignThread(){
-        
+    private volatile String responseMessage;
+
+    public ESignThread() {
     }
-    public String getResponseMessage(){
+
+    public String getResponseMessage() {
         return this.responseMessage;
     }
-    public String setJSONString(JSONObject object){
-        JSONString = object.toString();
+
+    public String setJSONString(JSONObject object) throws JSONException {
+        JSONString = object.toString(0);
         return JSONString;
     }
-    public void run(){
+
+    public void run() {
         String type = "application/json";
         URL u;
         try {
             u = new URL("https://stage-api.e-signlive.com/aws/rest/services/codejam");
             HttpURLConnection conn;
-        try {
-            conn = (HttpURLConnection) u.openConnection();
-            conn.setDoOutput(true);
-            conn.setRequestMethod( "POST" );
-            conn.setRequestProperty("Authorization", "Basic Y29kZWphbTpBRkxpdGw0TEEyQWQx"); 
-            conn.setRequestProperty( "Content-Type", type );
-            OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
-            writer.write(JSONString);
-            writer.close();
-            if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                String returnString = conn.getResponseMessage();
-                this.responseMessage = returnString;
-            } else {
-            // Server returned HTTP error code.
-                System.out.println("There's a problem with the response");
-            }
-             } catch (IOException ex) {
+            try {
+                conn = (HttpURLConnection) u.openConnection();
+                conn.setDoOutput(true);
+                conn.setDoInput(true);
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Authorization", "Basic Y29kZWphbTpBRkxpdGw0TEEyQWQx");
+                conn.setRequestProperty("Content-Type", type);
+                OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+                writer.write(JSONString);
+                writer.close();
+                InputStream input = conn.getInputStream();
+
+                if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    StringBuilder jb = new StringBuilder();
+                    String line = null;
+                    try {
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                        line = reader.readLine();
+                        this.responseMessage = line;
+                    } catch (Exception e) {
+                        System.out.println("Theres something wrong with the reader.");
+                    }
+
+                } else {
+                    // Server returned HTTP error code.
+                    System.out.println("There's a problem with the response");
+                }
+            } catch (IOException ex) {
                 Logger.getLogger(ESignThread.class.getName()).log(Level.SEVERE, null, ex);
-            } } catch (MalformedURLException ex) {
+            }
+        } catch (MalformedURLException ex) {
             Logger.getLogger(ESignThread.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
